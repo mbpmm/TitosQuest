23ed5146aae7ae0ab69a0c7640904dee292bb96e
mbpmm <marianomcr92@gmail.com>
2019-05-07 23:07:45 -0300

    Agregada la torreta, intento de raycast a enemigo, MonoBehaviourSingleton y GameManager

---
+++ TitoQuest/Assets/Game/Scripts/Boxes.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boxes : MonoBehaviour
+{
+    private GameObject turret;
+    private PlayerController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        turret = GameObject.Find("Sphere");
+        player = turret.GetComponent<PlayerController>();
+    }
+}
---
+++ TitoQuest/Assets/Game/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject cannon;
+    public LayerMask rayCastLayer;
+    public float rayDistance;
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(cannon.transform.position, cannon.transform.up, out hit, rayDistance, rayCastLayer))
+        {
+            Debug.DrawRay(cannon.transform.position, cannon.transform.up * hit.distance, Color.white);
+
+            string layerHitted = LayerMask.LayerToName(hit.transform.gameObject.layer);
+
+            switch (layerHitted)
+            {
+                case "enemy":
+                    if (hit.transform.gameObject.name == "Box")
+                    {
+                        Debug.DrawRay(cannon.transform.position, cannon.transform.up * hit.distance, Color.yellow);
+                        Debug.Log("Can Hit");
+                    }
+                    break;
+            }
+        }
+        else
+        {
+            Debug.DrawRay(cannon.transform.position, cannon.transform.up * rayDistance, Color.white);
+        }
+    }
+}
